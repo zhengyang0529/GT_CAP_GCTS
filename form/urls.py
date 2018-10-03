63ec10fdef61910831c16d38e418078019d44c92
Chris Chen <soulchris2@gmail.com>
2018-10-04 01:31:47 +0800

    Update Login page

--- form/urls.py
+++ form/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login
 
 urlpatterns = [
-    url(r'^$',views.login, name='login'),
+    url(r'^$',login,{'template_name': 'form/login.html'}),
 ]
