58842e2ccc353c5782ea396bed67bc8bca5344df
Chris Chen <soulchris2@gmail.com>
2018-10-05 00:42:56 +0800

    Create home page

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^login/', include('form.urls')),
+    url(r'^home/', include('report.urls')),
     url(r'^account/', include('accounts.urls'))
 ]
