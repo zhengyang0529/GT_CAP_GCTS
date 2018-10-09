988663b8352102c44dedbca5110c7cde6dd8f2d3
Chris Chen <soulchris2@gmail.com>
2018-10-09 14:51:24 +0800

    Update Login Page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$',views.home),
-    url(r'^login/$',login,{'template_name' : 'accounts/login.html'}),
+    url(r'^login/$',auth_views.LoginView.as_view(template_name = 'accounts/login.html'),name="login"),
 ]
