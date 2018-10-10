bae916bb17029e0a89067a9aee37173f5952bf5c
Ong Zheng Yang <ongzhengyang@Jewels-MacBook-Pro.local>
2018-10-10 23:58:32 +0800

    report django admin

--- report/models.py
+++ report/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Cell_Report(models.Model):
+    report = models.FileField(
+        upload_to='document/uploaded_file/',
+    )
+    created_dt = models.DateTimeField(
+        auto_now_add=True,
+        )
+    created_by = models.CharField(
+        max_length=225,
+        )
+    updated_dt = models.DateTimeField(
+        auto_now=True,
+        )
+    updated_by = models.CharField(
+        max_length=225,
+        )
+
